e9c4f078c846cee39a15659e2c9119647600089a
JMS <jms8167@gmail.com>
2016-02-18 17:02:04 +0900

    ch

--- cfc.c
+++ cfc.c
@@ -1,64 +1,64 @@
-//CFC 
+//CFC
 //cgi for c
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
 char src[65536];
 char compile[512];
-char name[512],out[512];
-char* path="/var/www/cgi-bin/";
+char cfile[512],out[512],param[512];
+char* path="/var/www/_FOLDER_/";
+void generate_file(char* name,char* prev_str){
+  do{
+    sprintf(name,"%s%s%d.c",path,prev_str,rand());
+  }while(!access(name,0));
+}
+void save_input(){
+    generate_file(param,"param");
+    FILE* fp=fopen(param,"w");
+    char in[65536]={0};
+    scanf("%s",in);
+    fprintf(fp,"%s",in);
+    fclose(fp);  
+}
+void chsrc(char* src_path,char* dst_path){
+    FILE* fp=fopen(src_path,"r");
+    FILE* dst=fopen(dst_path,"w");
+    fgets(src,65535,fp);        //#!usr/bin/cfcf/cfc
+    fgets(compile,511,fp);      //compile option
+    while(!feof(fp)){
+        memset(src,0,65536);
+        fgets(src,65536,fp);
+        fputs(src,dst);
+    }
+    fclose(fp);
+    fclose(dst);    
+}
 int main(int argc,char* argv[]){
-	
 	srand(time(0));
 	if(argc!=2){
 		puts("Usage : [C source]");
 		exit(EXIT_FAILURE);
 	}
-	char s[65536]={0};
-	int i=0;
-	scanf("%s",s);
-	char param[512];
-	sprintf(param,"%sparam%d",path,rand());
-	FILE* pp=fopen(param,"w");
-	fprintf(pp,"%s",s);
-	fclose(pp);
+    save_input();
 	int len= strlen(path);
-	int r = rand();
-	sprintf(name,"%sdst%d.c",path,r);
-	sprintf(out,"%sout%d",path,rand());
-	FILE* fp=fopen(argv[1],"r");
-	FILE* dst=fopen(name,"w");
-	fgets(src,65535,fp);
-	fgets(compile,511,fp);
-	while(!feof(fp)){
-		memset(src,0,65536);
-		fgets(src,65535,fp);
-		fputs(src,dst);
-	}
-	fclose(dst);
-	fclose(fp);
-	char cmd[512]={0};
+	generate_file(cfile,"src");	   
+	chsrc(argv[1],cfile);
+    generate_file(out,"out");
+    char cmd[512]={0};
 	//#gcc -o %s %s
-	sprintf(cmd,compile+1,out,name);
+	sprintf(cmd,compile+1,out,cfile);
 	system(cmd);
-	char curpath[512];
-//	fprintf(stderr,"currpath : %s\n",getcwd(curpath,511));
-//	fprintf(stderr,"gcc option : %s\n",cmd);
-//	chdir(path);
-	
-//	fprintf(stderr,"movepath : %s\n",getcwd(curpath,511));
 	sprintf(cmd,"./%s < %s",out+len,param);
 	system(cmd);
-	
 	sprintf(cmd,"rm %s",out+len);
 	system(cmd);
-	sprintf(cmd,"rm %s",name+len);
-	system(cmd);	
+	sprintf(cmd,"rm %s",cfile+len);
+	system(cmd);
 	sprintf(cmd,"rm %s",param+len);
 	system(cmd);
-	return 0;	
+	return 0;
 }
 /* Cfc usage
 Apache2에서 /var/www/cgi-bin 의 폴더안의 실행시킬 C 코드는
--- install.pl
+++ install.pl
@@ -1,15 +1,49 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+if($<!=0){
+    print "you must execute in sudo\n";
+    print "install cfc --NOT OK\n";
+    die;
+}
+print "what`s your folder name in /var/www/*/ ?";
+#changing dir /var/www/*/
+my $folder = <STDIN>;
+chomp $folder;
+open FP ,"<cfc.c";
+$/=undef;
+my $data=<FP>;
+$data =~ s/_FOLDER_/$folder/;
+close FP;
+open FP ,">cfc_out.c";
+print FP $data;
+close FP;
+#make debug interpreter
+open FP,"<cfc.c";
+$data=<FP>;
+$data =~ s/save_input(){/save_input(){return;/;
+close FP;
+open FP,">cfc_debug.c";
+print FP $data;
+close FP;
+
 if(-f "/usr/include/cfc_util.h"){
+    print "delete orgin file...\n";
 	system "rm /usr/include/cfc_util.h";
 }
+print "regist [cfc_util.h] in main C Std Lib...\n";
 system "sudo cp cfc_util.h /usr/include/";
 if(-d "/usr/bin/cfcf"){
+    print "delete origin dir...\n";
 	system "rm -r /usr/bin/cfcf";
 }
 system "mkdir /usr/bin/cfcf";
-system "cp cfc.c /usr/bin/cfcf";
+print "installing Lib...\n";
+system "mv cfc_out.c /usr/bin/cfcf";
+system "mv cfc_debug.c /usr/bin/cfcf";
 chdir "/usr/bin/cfcf";
-system "gcc -o cfc cfc.c";
+print "compiling Lib...\n";
+system "gcc -o cfc cfc_out.c";
+system "gcc -o cfcd cfc_debug.c";
 system "chmod 777 /usr/bin/cfcf";
+print "install cfc --OK\n";
